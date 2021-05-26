1a80c31d3bb1f6505f76d44d2c869634b6341e1e
Liviu Andrei <petrache.andrei1@gmail.com>
2021-05-26 17:26:42 +0300

    Switched to CLion for a moment

---
+++ CLionTester/cmake-build-debug/CMakeFiles/CLionTester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLionTester"
+  "CLionTester.pdb"
+  "CMakeFiles/CLionTester.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLionTester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLionTester/cmake-build-debug/CMakeFiles/CLionTester.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CLionTester.
+# This may be replaced when dependencies are built.
